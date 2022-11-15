f1ec069c9d211f29b0f4d8fc9a201c43f256d807
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2022-11-15 15:07:16 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,26 @@ import { Provider } from 'react-redux'
 import {store}from '../app/store'
 import StateContext from '../context/StateContext'
 import Layout from '../components/Layout'
+import { useEffect, useState } from 'react'
+import { FaUnsplash } from 'react-icons/fa'
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return <Provider store={store}>
    <StateContext>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+   {loading ? (
+          <div className="bg-slate-100 flex items-center justify-center w-[100vw] h-[100vh]">
+            <FaUnsplash className=" h-[8em] w-[8em]" />
+          </div>
+        ) : (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        )}
 </StateContext>
 </Provider>
 }
